02286d32f1eb8db0947b9268f6dbfe7acd911126
Gabri <rmbgabriel@gmail.com>
2021-10-29 10:45:39 +0100

    changed app text to Nodelicious!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Je suis Node!
+          Nodelicious!
         </p>
         <a
           className="App-link"
